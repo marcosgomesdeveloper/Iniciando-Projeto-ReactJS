1375673cd9198a235d0265d2a3636d281284c6ab
Marcos Gomes <marcos.gomes.developer@gmail.com>
2020-07-16 12:51:22 -0300

    Alteracoes no ambiente do projeto: It4us

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Projeto It4us com React JS</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Para começar, edite <code>src / App.js</code> e salve para recarregar.
         </p>
       </div>
     );
